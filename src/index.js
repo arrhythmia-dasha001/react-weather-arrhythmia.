d28998ebd35c7caa5aed428a648511b6cfd64cd8
arrhythmia-dasha001 <104265081+arrhythmia-dasha001@users.noreply.github.com>
2023-03-19 22:00:17 +0100

    added all

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+const container = document.querySelector("#weather");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
